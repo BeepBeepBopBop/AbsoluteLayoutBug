fa04a397ce416872f5db29f286c99e287f763334
BeepBeepBopBop <evan.carrere@gmail.com>
2024-02-26 13:24:15 +0100

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Maui.Layouts;
+using System.Diagnostics;
+
+namespace DemoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            SetLayoutBounds(0.2f, 0.8f);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            SetLayoutBounds(0.5f, 0.5f);
+        }
+
+        private void SetLayoutBounds(float greenBoxRatio, float blueBoxRatio)
+        {
+            double availableHeight = Height - topGrid.Height;
+            Rect greenRect = new Rect(0, 0, Width, availableHeight * greenBoxRatio);
+            Rect blueRect = new Rect(0, greenRect.Height, Width, availableHeight * blueBoxRatio);
+
+            absoluteLayout.SetLayoutBounds(greenBox, greenRect);
+            absoluteLayout.SetLayoutBounds(blueBox, blueRect);
+        }
+    }
+}
